0d7a7ab597f1c5a50951d7a546b84df36683931e
tatref <tatref@github.com>
2023-01-29 22:57:37 +0100

    add shm2pfns

---
+++ src/bin/shm2pfns.rs
@@ -0,0 +1,23 @@
+// Attach current process to shared memory segments from /proc/sysvipc/shm
+// root is required
+
+use std::collections::{HashMap, HashSet};
+
+use procfs::process::Pfn;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // (key, id) -> PFNs
+    let mut h: HashMap<(i32, u64), HashSet<Pfn>> = HashMap::new();
+
+    for shm in procfs::Shm::new().expect("Can't read /dev/sysvipc/shm") {
+        let pfns = snap::shm2pfns(&shm).expect("Got an error");
+
+        h.insert((shm.key, shm.shmid), pfns);
+    }
+
+    for (&(k, id), v) in h.iter() {
+        println!("key: {k}, id: {id}: {} PFNs", v.len());
+    }
+
+    Ok(())
+}
