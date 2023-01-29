a713f6a2ae3bd41495ba1025aa4811c674d34816
tatref <tatref@github.com>
2023-01-29 03:02:00 +0100

    chore

--- oracle-tools/src/bin/connections.rs
+++ oracle-tools/src/bin/connections.rs
@@ -1,5 +1,6 @@
 use oracle::{Connection, Error};
 use std::env;
+use std::io::Write;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<_> = env::args().collect();
@@ -14,11 +15,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let wait = args[6].parse().unwrap();
 
     let connections: Vec<Connection> = (0..max_conn)
-        .map(|i| Connection::connect(user, password, format!("{}:1521/{}", host, service)).unwrap())
+        .map(|i| {
+            if i % 10 == 0 {
+                print!("{}/{}\r", i, max_conn);
+                std::io::stdout().lock().flush();
+            }
+            Connection::connect(user, password, format!("{}:1521/{}", host, service)).unwrap()
+        })
         .collect();
+    println!("\n{} connections", max_conn);
 
-    println!("{} connections", max_conn);
-
+    println!("Sleeping for {}s", wait);
     std::thread::sleep(std::time::Duration::from_secs(wait));
 
     Ok(())
