48e77305ec50710cdbe98ce6306b9fe269cdd62d
tatref <tatref@github.com>
2024-02-13 01:23:11 +0100

    update archive

--- make_archive.sh
+++ make_archive.sh
@@ -24,14 +24,18 @@ cargo clean
 cargo update
 RUSTFLAGS="-C target-cpu=x86-64-v2" cargo zigbuild --release --target x86_64-unknown-linux-gnu.2.12
 
+cross b --release --target x86_64-pc-windows-gnu --bin kpageflags-viewer
+
 echo "$LONG_VERSION" > VERSION
 
 ARCHIVE="linux-mem-$SHORT_VERSION.tar.xz"
 tar -cJf "$ARCHIVE" \
   --transform 's:target/x86_64-unknown-linux-gnu/release/::' \
+  --transform 's:target/x86_64-pc-windows-gnu/release/::' \
   --transform "s:^:linux-mem-$SHORT_VERSION/:" \
   README.md VERSION \
   target/x86_64-unknown-linux-gnu/release/{memstats,procinfo,shmem,kpageflags-viewer} \
+  target/x86_64-pc-windows-gnu/release/kpageflags-viewer.exe \
   proc_snap/snap.py
 
 echo Done
